22aaa367d37f14ddc0b780c4d6fd6a2f66f29402
Venance <contact@venanceimo.com>
2026-07-05 14:00:58 +0000

    chore: remove final unused next/navigation imports

--- app/(locataire)/locataire/[id]/page.tsx
+++ app/(locataire)/locataire/[id]/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { 
   CalendarClock, 
   Download, 
--- app/(locataire)/locataire/[id]/paiements/page.tsx
+++ app/(locataire)/locataire/[id]/paiements/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState, useMemo } from "react";
-import { useParams, useRouter } from "next/navigation";
+import { useParams } from "next/navigation";
 import { 
   FileText,
   Download,
