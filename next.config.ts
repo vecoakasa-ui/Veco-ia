65a8cfa51c80d00ce371041024c33fe634c0074b
Venance <contact@venanceimo.com>
2026-07-06 17:21:13 +0000

    chore: add serveo origins to next.config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['2523c1b8fbff9607-102-208-128-155.serveousercontent.com', 'veco-ia-test.loca.lt'],
 };
 
 export default nextConfig;
